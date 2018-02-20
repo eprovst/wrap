d328d015d1b8303d69e7890b7ae2dfeff0538078
Evert Provoost <evert.provoost@gmail.com>
2018-02-20 07:36:25 +0100

    Add basic window

--- wrap.go
+++ wrap.go
@@ -8,9 +8,18 @@ import (
 	"os"
 
 	"github.com/Wraparound/wrap/cli"
+	"github.com/Wraparound/wrap/gui"
 )
 
 func main() {
+	// Is the gui flag set? TODO: Move this into the root command?
+	for _, arg := range os.Args {
+		if arg == "--gui" {
+			gui.DrawWindow()
+			os.Exit(0)
+		}
+	}
+
 	// Run the root command
 	if err := cli.WrapCmd.Execute(); err != nil {
 		fmt.Println(err)
