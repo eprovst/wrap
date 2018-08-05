3534490b8d554613c507df3faf0ceb127b7fa338
Evert Provoost <evert.provoost@gmail.com>
2018-08-05 22:19:55 +0200

    Redesigned stageplay

--- pdf/stageplay.go
+++ pdf/stageplay.go
@@ -4,10 +4,9 @@ package pdf
 
 var stageplay = aTheme{
 	action: lineStyle{
+		Indent:        12.5 * en,
+		LineLenght:    33,
 		LeadingBefore: 1,
-		Indent:        10 * en,
-		LineLenght:    50,
-		Italics:       true,
 	},
 	act: lineStyle{
 		LeadingBefore: 0,
@@ -19,114 +18,109 @@ var stageplay = aTheme{
 	slugLine: lineStyle{
 		Indent:        25 * en,
 		LineLenght:    35,
-		LeadingBefore: 2,
+		LeadingBefore: 1,
 		Underline:     true,
 	},
 	centeredText: lineStyle{
 		LeadingBefore: 1,
-		Indent:        10 * en,
-		LineLenght:    50,
-		Italics:       true,
+		LineLenght:    60,
 		Centered:      true,
 	},
 	looseLyrics: lineStyle{
 		LeadingBefore: 1,
 		LineLenght:    60,
-		AllCaps:       true,
 		Italics:       true,
 	},
 	transition: lineStyle{
+		Indent:        25 * en,
+		LineLenght:    35,
 		LeadingBefore: 1,
-		FlushRight:    true,
+		AllCaps:       true,
 	},
 
 	character: lineStyle{
+		Indent:        25 * en,
+		LineLenght:    35,
 		LeadingBefore: 1,
 	},
 	parenthetical: lineStyle{
-		FirstLineIndent: 14 * en,
-		Indent:          15 * en,
-		LineLenght:      45,
+		Indent:     12.5 * en,
+		LineLenght: 33,
 	},
 	dialogue: lineStyle{
-		Indent:     5 * en,
-		LineLenght: 55,
+		LineLenght: 60,
 	},
 	lyrics: lineStyle{
-		Indent:     5 * en,
-		LineLenght: 55,
-		AllCaps:    true,
+		LineLenght: 60,
 		Italics:    true,
 	},
 
 	dualCharacterOne: lineStyle{
 		LeadingBefore: 1,
-		LineLenght:    28,
+		Indent:        8 * en,
+		LineLenght:    20,
 	},
 	dualParentheticalOne: lineStyle{
-		FirstLineIndent: 9 * en,
-		Indent:          10 * en,
-		LineLenght:      18,
+		Indent:     3.5 * en,
+		LineLenght: 18,
 	},
 	dualDialogueOne: lineStyle{
-		Indent:     5 * en,
-		LineLenght: 23,
+		LineLenght: 28,
 	},
 	dualLyricsOne: lineStyle{
-		Indent:     5 * en,
-		LineLenght: 23,
-		AllCaps:    true,
+		LineLenght: 28,
 		Italics:    true,
 	},
 
 	dualCharacterTwo: lineStyle{
 		LeadingBefore: 1,
-		Indent:        32 * en,
-		LineLenght:    28,
+		Indent:        40 * en,
+		LineLenght:    20,
 	},
 	dualParentheticalTwo: lineStyle{
-		FirstLineIndent: 41 * en,
-		Indent:          42 * en,
-		LineLenght:      17,
+		Indent:     35.5 * en,
+		LineLenght: 18,
 	},
 	dualDialogueTwo: lineStyle{
-		Indent:     37 * en,
+		Indent:     32 * en,
 		LineLenght: 28,
 	},
 	dualLyricsTwo: lineStyle{
-		Indent:     37 * en,
+		Indent:     32 * en,
 		LineLenght: 28,
-		AllCaps:    true,
 		Italics:    true,
 	},
 
 	more: lineStyle{
-		Indent: 20 * en,
+		Indent: 25 * en,
 	},
 
 	titlePageTitle: lineStyle{
-		LeadingBefore: 20,
-		Indent:        rightMargin - leftMargin,
-		Centered:      true,
+		LeadingBefore: 15,
+		Indent:        25 * en,
+		LineLenght:    35,
+		Underline:     true,
+		AllCaps:       true,
 	},
 	titlePageSubtitle: lineStyle{
-		Indent:   rightMargin - leftMargin,
-		Centered: true,
+		LeadingBefore: 1,
+		Indent:        25 * en,
+		LineLenght:    35,
 	},
 	titlePageCredit: lineStyle{
 		LeadingBefore: 1,
-		Indent:        rightMargin - leftMargin,
-		Centered:      true,
+		Indent:        25 * en,
+		LineLenght:    35,
 	},
 	titlePageAuthor: lineStyle{
 		LeadingBefore: 1,
-		Indent:        rightMargin - leftMargin,
-		Centered:      true,
+		Indent:        25 * en,
+		LineLenght:    35,
 	},
 	titlePageSource: lineStyle{
-		LeadingBefore: 2,
-		Indent:        rightMargin - leftMargin,
-		Centered:      true,
+		LeadingBefore: 1,
+		Indent:        25 * en,
+		LineLenght:    35,
 	},
 
 	titlePageRight: lineStyle{
